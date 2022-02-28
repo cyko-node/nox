20f16d764f6a0946a396d86fa502a8877c066429
eggheadedmonkey <eggheadedmonkey@gmail.com>
2022-02-28 23:27:01 +0100

    ...

--- main.d.ts
+++ main.d.ts
@@ -1,22 +1,53 @@
-/*
+/**
  * Cyko: **N**ode **O**bjective e**X**tensions
  *
  * ---
  *
- * Collection of simple and _safe_ object utilities.
+ * Collection of simple and safe objective utilities.
  *
- * - Property queries
- * - Property access
+ * - Object property queries
+ * - Object property access
+ *
+ * - Value conversions
  *
  * ---
  *
  * @author eggheadedmonkey <cyko@eggheadedmonkey.com>
  */
 
-declare module "nox..." {
+declare module "nox" {
 
   /**
-   * `object` has a **property** with the specified `name` ?
+   * Objective representation of the **package.json** contents.
+   *
+   * ```
+   * // object structure
+   * interface example {
+   *   name: string;
+   *   version: string;
+   *   description: string;
+   *   main: string;
+   *   private: boolean;
+   *   keywords: string[ ... ];
+   *   license: string;
+   *   author: string|person;
+   *   scripts: { ...; };
+   *   dependencies: { ...; };
+   *   repository: { type: string; url: string; };
+   *   maintainers: [ string|person, ... ];
+   *   contributors: [ string|person, ... ];
+   * }
+   * // author, maintainers, contributors
+   * interface person { name: string; email: string; }
+   * ```
+   */
+
+  function package(): object
+
+  /* ----------------------------------------------------------------------- */
+
+  /**
+   * `object` has a property with the specified `name` ?
    *
    * ---
    *
@@ -27,7 +58,7 @@ declare module "nox..." {
   function has(obj: object, key: string): boolean
 
   /**
-   * `object` has a **method** with the specified `name` ?
+   * `object` has a functional property (method) with the specified `name` ?
    *
    * ---
    *
@@ -42,8 +73,8 @@ declare module "nox..." {
    *
    * ---
    *
-   * Selects `object['name']` or `default` based upon weather the `object`
-   * {@link has has} a property with the specified `name`.
+   * Selects between `object[name]` and `default` based upon weather the
+   * `object` {@link has has} a property with the specified `name`.
    *
    * ---
    *
@@ -52,6 +83,37 @@ declare module "nox..." {
    * @param def `default` = `null`
    */
 
-  function get(obj: object, key: string, def: any): any
+  function get(obj: object, key: string, def: any = null): any
+
+  /* ----------------------------------------------------------------------- */
+
+  /**
+   * Convert a `value` of *any* type to a **string**.
+   *
+   * ---
+   *
+   * @param val `value`
+   */
+
+  function string(val: any): string
+
+  /**
+   * Convert a `value` of *any* type to an **integer**.
+   *
+   * ```
+   * nox.int('abc')      //  NaN
+   * nox.int('123abc')   //  NaN
+   * nox.int('123')      //  123
+   * nox.int('-321')     // -321
+   * nox.int('123.321')  //  123
+   * nox.int('Infinity') //  Infinity
+   * ```
+   *
+   * ---
+   *
+   * @param val `value`
+   */
+
+  function int(val: any): number
 
 }
--- main.js
+++ main.js
@@ -1,99 +1,33 @@
 const pkg = require('./package.json')
 
-/**
- * Get an (r/o) objective representation of the **package.json** contents.
- */
+/* ------------------------------------------------------------------------- */
 
 exports.package = function() {
   return pkg
 }
 
-/**
-  * `object` has a property with the specified `name` ?
-  *
-  * ---
-  *
-  * @param {object} obj `object`
-  * @param {string} key `name`
-  */
+/* ------------------------------------------------------------------------- */
 
 exports.has = function(obj, key) {
   return Object.keys(obj).includes(key)
 }
 
-/**
- * `object` has a property (method) with the specified `name` ?
- *
- * ---
- *
- * @param {object} obj `object`
- * @param {string} key `name`
- */
-
 exports.fas = function(obj, key) {
   return Object.prototype.hasOwnProperty.call(obj, key) ? true : false
 }
-
-/**
- * Get the `object` property `name`.
- *
- * ---
- *
- * Selects `object[name]` or `default` based upon weather the `object`
- * {@link exports.has has} a property with the specified `name`.
- *
- * ---
- *
- * @param {object}  obj  `object`
- * @param {string}  key  `name`
- * @param {*|null} [def] `default` = `null`
- */
 
 exports.get = function(obj, key, def = null) {
   return this.has(obj, key) ? obj[key] : def
 }
 
+/* ------------------------------------------------------------------------- */
 
-
-/*
-const o = exports.o = {
-
-  /**
-   * `object` has a **property** with the specified `name`?
-   * @param {object} obj `object`
-   * @param {string} key `name`
-   * @returns {boolean} `true/false`
-   *
-
-  has: function(obj, key) {
-    return Object.keys(obj).includes(key)
-  },
-
-  /**
-   * `object` has a **method** with the specified `name`.
-   * @alias module:nox.fas
-   * @param {object} obj `object`
-   * @param {string} key `name`
-   * @returns {boolean} `true/false`
-   *
-
-  fas: function(obj, key) {
-    return Object.prototype.hasOwnProperty.call(obj, key)
-  },
-
-  /**
-   * Get `object` property `name`.
-   *
-   * Selects between `object[name]` and `default` based upon weather the
-   * `object` {@link o.has `has`} a property with the specified `name`.
-   * @param {object} obj  `object`
-   * @param {string} key  `name`
-   * @param {*|null}[def] `default` : `null`
-   *
-
-  get: function(obj, key, def = null) {
-    return this.has(obj, key) ? obj[key] : def
-  },
-
+exports.string = function(val) {
+  const x = String(val)
+  return x
 }
-*/
+
+exports.int = function(val) {
+  const x = Number(val);
+  return x >= 0 ? Math.floor(val) : Math.ceil(val);
+}
